9be264e6f9e7939f27745915859e5e2d4320ddcc
bhartishelpinghands <114795408+bhartishelpinghands@users.noreply.github.com>
2022-10-03 04:00:18 -0400

    Update chains.ts

--- chains.ts
+++ chains.ts
@@ -1,16 +1,4 @@
-import type { AddEthereumChainParameter } from '@web3-react/types'
 
-const ETH: AddEthereumChainParameter['nativeCurrency'] = {
-  name: 'Ether',
-  symbol: 'ETH',
-  decimals: 18,
-}
-
-const MATIC: AddEthereumChainParameter['nativeCurrency'] = {
-  name: 'Matic',
-  symbol: 'MATIC',
-  decimals: 18,
-}
 
 interface BasicChainInformation {
   urls: string[]
@@ -28,20 +16,7 @@ function isExtendedChainInformation(
   return !!(chainInformation as ExtendedChainInformation).nativeCurrency
 }
 
-export function getAddChainParameters(chainId: number): AddEthereumChainParameter | number {
-  const chainInformation = CHAINS[chainId]
-  if (isExtendedChainInformation(chainInformation)) {
-    return {
-      chainId,
-      chainName: chainInformation.name,
-      nativeCurrency: chainInformation.nativeCurrency,
-      rpcUrls: chainInformation.urls,
-      blockExplorerUrls: chainInformation.blockExplorerUrls,
-    }
-  } else {
-    return chainId
-  }
-}
+
 
 export const CHAINS: { [chainId: number]: BasicChainInformation | ExtendedChainInformation } = {
   1: {
