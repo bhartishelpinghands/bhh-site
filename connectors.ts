1e3ed6a0fc8f204131f97a61da71cc7f13c5d08d
bhartishelpinghands <114795408+bhartishelpinghands@users.noreply.github.com>
2022-10-03 04:37:48 -0400

    Update connectors.ts

--- connectors.ts
+++ connectors.ts
@@ -1,27 +1,6 @@
 import { InjectedConnector } from '@web3-react/injected-connector';
-import { Network } from '@web3-react/network'
-import type { Networkish } from '@ethersproject/networks'
-import type { BaseProvider, Web3Provider } from '@ethersproject/providers'
-import { createWeb3ReactStoreAndActions } from '@web3-react/store'
-import type { Actions, Connector, Web3ReactState, Web3ReactStore } from '@web3-react/types'
-import { useEffect, useMemo, useState } from 'react'
-import type { EqualityChecker, UseBoundStore } from 'zustand'
-import create from 'zustand'
-
-function initializeConnector<T extends Connector>(
-  f: (actions: Actions) => T
-): [T, Web3ReactHooks, Web3ReactStore] {
-  const [store, actions] = createWeb3ReactStoreAndActions()
-
-  const connector = f(actions)
-  const useConnector = create(store)
-
-  const stateHooks = getStateHooks(useConnector)
-  const derivedHooks = getDerivedHooks(stateHooks)
-  const augmentedHooks = getAugmentedHooks<T>(connector, stateHooks, derivedHooks)
-
-  return [connector, { ...stateHooks, ...derivedHooks, ...augmentedHooks }, store]
-}
+import { Network } from '@web3-react/network';
+import { initializeConnector } from '@web3-react/core';
 
 export const injected = new InjectedConnector({
   supportedChainIds: [1, 42161],
