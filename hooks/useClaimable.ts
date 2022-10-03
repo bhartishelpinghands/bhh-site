a63dd7f110bd96f9c7135a48a03bae0f87c09cbe
bhartishelpinghands <114795408+bhartishelpinghands@users.noreply.github.com>
2022-10-03 02:22:38 -0400

    Revert "Update useClaimable.ts"
    This reverts commit 3f9fb22f9698fffb70155e2eba5c542fd9806fd9.

--- hooks/useClaimable.ts
+++ hooks/useClaimable.ts
@@ -4,11 +4,12 @@ import useKeepSWRDataLiveAsBlocksArrive from "./useKeepSWRDataLiveAsBlocksArrive
 import useRewardTracker from "./useRewardTracker";
 
 function getClaimable(contract: RewardTracker, vaultaddress) {
+  return async (_: string) => {
     const balance = await contract.claimable(vaultaddress);
 
     return balance;
   };
-
+}
 
 export default function useClaimable(
   trackerAddress: string,
