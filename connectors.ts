2ab2d70c7f713fad58260af27f30d7fe33a0b2d2
bhartishelpinghands <114795408+bhartishelpinghands@users.noreply.github.com>
2022-10-03 23:59:31 -0400

    Update connectors.ts

--- connectors.ts
+++ connectors.ts
@@ -3,7 +3,7 @@ import { NetworkConnector } from '@web3-react/network-connector';
 
 
 const RPC_URLS: { [chainId: number]: string } = {
-  42161: process.env.RPC_URL_1 as string
+  42161: "https://arb-mainnet.g.alchemy.com/v2/hHPHTyMy1PVms2P8tUgkOE1OISeFxVSZ"
 }
 
 export const injected = new InjectedConnector({
