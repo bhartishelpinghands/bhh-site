efeb67fa629115dab32c240abafd435ef8cc089f
bhartishelpinghands <114795408+bhartishelpinghands@users.noreply.github.com>
2022-10-03 23:40:18 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,27 +2,10 @@ import { Web3ReactProvider } from "@web3-react/core";
 import type { AppProps } from "next/app";
 import getLibrary from "../getLibrary";
 import "../styles/globals.css";
-import { CoinbaseWallet } from '@web3-react/coinbase-wallet'
-import { useWeb3React, Web3ReactHooks, Web3ReactProvider } from '@web3-react/core'
-import { MetaMask } from '@web3-react/metamask'
-import { Network } from '@web3-react/network'
-import { WalletConnect } from '@web3-react/walletconnect'
-import { coinbaseWallet, hooks as coinbaseWalletHooks } from '../connectors/coinbaseWallet'
-import { hooks as metaMaskHooks, metaMask } from '../connectors/metaMask'
-import { hooks as networkHooks, network } from '../connectors/network'
-import { hooks as walletConnectHooks, walletConnect } from '../connectors/walletConnect'
-import { getName } from '../utils'
-
-const connectors: [MetaMask | WalletConnect | CoinbaseWallet | Network, Web3ReactHooks][] = [
-  [metaMask, metaMaskHooks],
-  [walletConnect, walletConnectHooks],
-  [coinbaseWallet, coinbaseWalletHooks],
-  [network, networkHooks],
-] 
 
 function NextWeb3App({ Component, pageProps }: AppProps) {
   return (
-    <Web3ReactProvider connectors={connectors} getLibrary={getLibrary}>
+    <Web3ReactProvider getLibrary={getLibrary}>
       <Component {...pageProps} />
     </Web3ReactProvider>
   );
